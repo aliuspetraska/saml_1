61e5c12c7ac37d66ddfc711287db09d39a31128b
Alius Petraska <alius.petraska@ibm.com>
2021-09-16 16:54:03 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const rateLimit = require('express-rate-limit');
 const secure = require('express-secure-only');
 const compression = require('compression');
+const passport = require('passport');
 const express = require('express');
 const nocache = require('nocache');
 const helmet = require('helmet');
 const http = require('http');
-// const path = require('path');
+const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+const SamlStrategy = require('passport-saml').Strategy;
 
 const app = express();
 
@@ -29,6 +32,40 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+
+const strategy = new SamlStrategy({
+  path: '/login/callback',
+  entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+  issuer: 'passport-saml',
+  cert: fs.readFileSync(path.join(__dirname, 'idp.crt'), 'utf8'),
+  signatureAlgorithm: 'sha256',
+}, (profile, done) => {
+  return done(null, profile);
+});
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+
+app.get('/login', (req, res) => {
+  passport.authenticate('saml', { successRedirect: '/', failureRedirect: '/error', failureFlash: true });
+});
+
+app.post('/login/callback', (req, res) => {
+  passport.authenticate('saml', { failureRedirect: '/error', failureFlash: true }), (req, res) => {
+    return res.redirect('/success');
+  };
+});
+
+app.get('/success', (req, res) => {
+  return res.status(200).json({ status: 'Success' });
+});
+
+app.get('/error', (req, res) => {
+  return res.status(200).json({ status: 'Error' });
+});
 
 app.use('/', (req, res) => {
   return res.status(200).json({ status: 'Works' });
